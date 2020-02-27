5ba71738f09da5d6fdaa192557a189e1959d8205
Floyd Mann <floyd@affinitybridge.com>
2020-02-27 11:06:09 -0800

    Adding Fission FieldFormatter, settings options.

--- src/Form/IpfsSettingsForm.php
+++ src/Form/IpfsSettingsForm.php
@@ -32,12 +32,54 @@ class IpfsSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('ipfs.settings');
 
+    $form['ipfs_gateway_type'] = [
+      '#type' => 'radios',
+      '#title' => t('IPFS Gateway Type'),
+      '#default_value' => $config->get('ipfs_gateway_type'),
+      '#options' => ['fission' => $this->t('Fission'), 'ipfs' => $this->t('IPFS')],
+      '#description' => $this->t('Which type of IPFS Gateway to use.'),
+    ];
+
     $form['ipfs_host'] = [
       '#type' => 'url',
       '#title' => $this->t("IPFS API Server"),
       '#description' => $this->t("The URL of the IPFS API server, for example: http://127.0.0.1:5001."),
-      '#required' => TRUE,
+      '#required' => FALSE,
       '#default_value' => $config->get('ipfs_host'),
+      '#states' => [
+        'visible' => [
+          ':input[name=ipfs_gateway_type]' => ['value' => 'ipfs'],
+        ],
+      ],
+    ];
+
+    $form['fission'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Fission settings'),
+      '#states' => [
+        'visible' => [
+          ':input[name=ipfs_gateway_type]' => ['value' => 'fission'],
+        ],
+      ],
+    ];
+
+    $form['fission']['fission_gateway'] = [
+      '#type' => 'url',
+      '#title' => $this->t("Fission Gateway"),
+      '#description' => $this->t("The URL of the Fission Gateway, for example: https://ipfs.runfission.com."),
+      '#default_value' => $config->get('fission_gateway')
+    ];
+    $form['fission']['fission_username'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t("Username"),
+      '#description' => $this->t("Your Fission username."),
+      '#default_value' => $config->get('fission_username')
+    ];
+    $form['fission']['fission_password'] = [
+      '#type' => 'password',
+      '#title' => $this->t("Password"),
+      '#description' => $this->t("Your Fission password."),
+      '#default_value' => $config->get('fission_password')
     ];
 
     return parent::buildForm($form, $form_state);
@@ -48,10 +90,15 @@ class IpfsSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('ipfs.settings')
-      ->set('ipfs_host', $form_state->getValue('ipfs_host'));
+      ->set('ipfs_gateway_type', $form_state->getValue('ipfs_gateway_type'))
+      ->set('ipfs_host', $form_state->getValue('ipfs_host'))
+      ->set('fission_gateway', $form_state->getValue('fission_gateway'))
+      ->set('fission_username', $form_state->getValue('fission_username'));
 
+    if ($form_state->getValue('fission_password')) {
+      $config->set('fission_password', $form_state->getValue('fission_password'));
+    }
     $config->save();
-
     parent::submitForm($form, $form_state);
   }
 
---
+++ src/Plugin/Field/FieldFormatter/IpfsFissionImageFormatter.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Drupal\ipfs\Plugin\Field\FieldFormatter;
+
+use Drupal\file\FileInterface;
+use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
+use Drupal\ipfs\IpfsHandler;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\image\Entity\ImageStyle;
+
+/**
+ * Plugin implementation of the 'ipfs_fission_image_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "ipfs_fission_image_formatter",
+ *   label = @Translation("IPFS Fission image formatter"),
+ *   field_types = {
+ *     "image"
+ *   }
+ * )
+ */
+class IpfsFissionImageFormatter extends ImageFormatter {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs an ImageFormatter object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings settings.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $image_style_storage
+   *   The entity storage for the image.
+   * @param \Drupal\ipfs\IpfsHandler $ipfsHandler
+   *   IPFS Handler service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, AccountInterface $current_user, EntityStorageInterface $image_style_storage, ConfigFactoryInterface $config_factory) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $current_user, $image_style_storage);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('current_user'),
+      $container->get('entity.manager')->getStorage('image_style'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    $files = $this->getEntitiesToView($items, $langcode);
+
+    // Early opt-out if the field is empty.
+    if (empty($files)) {
+      return $elements;
+    }
+
+    $url = NULL;
+    $image_link_setting = $this->getSetting('image_link');
+    // Check if the formatter involves a link.
+    if ($image_link_setting == 'content') {
+      $entity = $items->getEntity();
+      if (!$entity->isNew()) {
+        $url = $entity->urlInfo();
+      }
+    }
+    elseif ($image_link_setting == 'file') {
+      $link_file = TRUE;
+    }
+
+    $image_style_setting = $this->getSetting('image_style');
+
+    // Collect cache tags to be added for each item in the field.
+    $base_cache_tags = [];
+    if (!empty($image_style_setting)) {
+      $image_style = $this->imageStyleStorage->load($image_style_setting);
+      $base_cache_tags = $image_style->getCacheTags();
+    }
+
+    foreach ($files as $delta => $file) {
+      $cache_contexts = [];
+      if (isset($link_file)) {
+        $image_uri = $file->getFileUri();
+
+        // @todo Wrap in file_url_transform_relative(). This is currently
+        // impossible. As a work-around, we currently add the 'url.site' cache
+        // context to ensure different file URLs are generated for different
+        // sites in a multisite setup, including HTTP and HTTPS versions of the
+        // same site. Fix in https://www.drupal.org/node/2646744.
+        $url = Url::fromUri(file_create_url($image_uri));
+        $cache_contexts[] = 'url.site';
+      }
+      $cache_tags = Cache::mergeTags($base_cache_tags, $file->getCacheTags());
+      $fileUri = $file->getFileUri();
+
+      if ('ipfs' == \Drupal::service('stream_wrapper_manager')->getScheme($fileUri)) {
+        $uri = $this->configFactory->get('ipfs.settings')->get('fission_gateway') . '/ipfs/';
+        $uri .= str_replace('ipfs://', '', $fileUri);
+        $file->setFileUri($uri);
+      }
+
+
+      // Extract field item attributes for the theme function, and unset them
+      // from the $item so that the field template does not re-render them.
+      $item = $file->_referringItem;
+      $item_attributes = $item->_attributes;
+      unset($item->_attributes);
+
+
+      $elements[$delta] = [
+        '#theme' => 'image_formatter',
+        '#item' => $item,
+        '#item_attributes' => $item_attributes,
+        '#image_style' => $image_style_setting,
+        '#url' => $url,
+        '#cache' => [
+          'tags' => $cache_tags,
+          'contexts' => $cache_contexts,
+        ],
+      ];
+
+    }
+
+    return $elements;
+  }
+}
